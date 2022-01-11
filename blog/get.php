a5e43c62c0afdf58686c7917d7a034a63c1756ef
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-01-11 15:00:59 +0900

    Fix Blog not renderred

--- blog/get.php
+++ blog/get.php
@@ -1,11 +1,11 @@
 <?php
 	header("Content-Type: Application/json; charset=UTF-8");
 	$pageNum = $_GET["page"];
-	$dataJSON = file_get_contents('./data.json');
+	$dataJSON = file_get_contents(__DIR__.'/data.json');
 	$dataJSON = json_decode($dataJSON, true);
 	$ret = array();  // Return
 	$now = ($pageNum - 1) * 5;
-	for($i=$now; $i<$now+5; $i++){
+	for($i = $now; $i < $now + 5; $i++){
 		if($i < count($dataJSON)){
 			$ret[] = $dataJSON[$i];
 		}
